b7ae721473ef92a293255311a042fac1765ceaf9
icha <ellaainurriska1@gmail.com>
2025-05-09 20:19:43 +0700

    buat struktur awal halaman HomePage dengan AppBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,101 +1,23 @@
 import 'package:flutter/material.dart';
 
-class TodoHomePage extends StatefulWidget {
-  const TodoHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<TodoHomePage> createState() => TodoHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class TodoHomePageState extends State<TodoHomePage> {
-  final List<String> _todos = []; // Daftar tugas
-  final TextEditingController _controller = TextEditingController(); // Untuk input teks
-
-  void _addTodo() {
-    final text = _controller.text;
-    if (text.isNotEmpty) {
-      setState(() {
-        _todos.add(text);
-        _controller.clear();
-      });
-    }
-  }
-
-  // void _removeTodo(int index) {
-  //   setState(() {
-  //     _todos.removeAt(index);
-  //   });
-  // }
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepPurple.shade300,
       appBar: AppBar(
-        title: const Text('daftar Tugas'),
+        title: const Text("Daftar Tugas"),
         centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemCount: _todos.length,
-              itemBuilder: (context, index) {
-                return Dismissible(key: Key(_todos[index]),
-                direction: DismissDirection.endToStart,
-                onDismissed: (direction) {
-                  setState(() {
-                    _todos.removeAt(index);
-                  });
-
-                  // ScaffoldMessenger.of(context).showSnackBar(
-                  //     const SnackBar(
-                  //       content: Text('Delete'),
-                  //     ),
-                  //   );
-                  },
-                 background: Container(
-                  color: Colors.red,
-                  alignment: Alignment.centerRight,
-                  padding: const EdgeInsets.all(20),
-                  child: const Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                ),
-                child: Card(
-                    child: ListTile(
-                      title: Text(_todos[index]),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-         SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      labelText: 'Tambahkan tugas',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: _addTodo,
-                  child: const Text('Tambah'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    )
-   );
+    );
   }
 }
