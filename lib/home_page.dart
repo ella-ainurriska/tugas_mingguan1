4b2d3d6ac81fe91c69c2c1b2b3f0edbae1bfef22
icha <ellaainurriska1@gmail.com>
2025-05-09 20:42:35 +0700

    tambahkan input TextField dan tombol tambah tugas

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +19,33 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
         foregroundColor: Colors.white,
+      ),
+      floatingActionButton: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'tulis daftar tugas',
+                  filled: true,
+                  fillColor: Colors.deepPurple.shade200,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.deepPurple,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            )
+           )
+           FloatingActionButton(
+            onPressed: saveNewTask,
+            child: const Icon(Icons.add),
+            ),
+        ],
       ),
     );
   }
